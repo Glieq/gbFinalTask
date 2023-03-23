0f3a13e79e87c17fe737e47d9a7ddca3f544acf6
Gleb <phantomj@mail.ru>
2023-03-23 14:05:24 +0300

    The program that solves the problem

---
+++ task/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+string[] array2 = { "Russia", "Kazan", "Denmark" };
+
+string[] clearArray(string[] mas)
+{
+    int count = 0;
+    int countForArr = 0;
+    int countI = 0;
+    while (count < mas.Length)
+    {
+        if (mas[count].Length < 4)
+        {
+            countForArr++;
+        }
+        count++;
+    }
+    count = 0;
+    string[] newArr = new string[countForArr];
+    while (count < mas.Length)
+    {
+        if (mas[count].Length < 4)
+        {
+            newArr[countI] = mas[count];
+            countI++;
+        }
+        count++;
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", newArr));
+    return newArr;
+}
+
+
+clearArray(array);
+clearArray(array2);
+
